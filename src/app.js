22c06cf8155fd0c05c136814f109b4dc73c54427
Luis Camargo <luismiguelcamar@gmail.com>
2022-10-06 02:54:01 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["dog", "co-worker", "neighbor", "fridge"];
+  let action = ["ate", "hacked", "lost", "broke"];
+  let which = ["my homework", "my leg", "your car", "my phone"];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    "My" +
+    " " +
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    which[Math.floor(Math.random() * 4)];
+
   console.log("Hello Rigo from the console!");
 };
