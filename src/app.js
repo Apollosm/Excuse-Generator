34443a4d22c2226ad402943364e6f63c21db590c
Luis Camargo <luismiguelcamar@gmail.com>
2022-10-06 02:59:32 +0000

    Added 'when'

--- src/app.js
+++ src/app.js
@@ -9,7 +9,13 @@ window.onload = function() {
   //write your code here
   let who = ["dog", "co-worker", "neighbor", "fridge"];
   let action = ["ate", "hacked", "lost", "broke"];
-  let which = ["my homework", "my leg", "your car", "my phone"];
+  let what = ["my homework", "my leg", "your car", "my phone"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch"
+  ];
 
   let excuseP = document.querySelector("#excuse");
   excuseP.textContent =
@@ -19,7 +25,11 @@ window.onload = function() {
     " " +
     action[Math.floor(Math.random() * 4)] +
     " " +
-    which[Math.floor(Math.random() * 4)];
+    what[Math.floor(Math.random() * 4)] +
+    " " +
+    when[Math.floor(Math.random() * 4)];
 
   console.log("Hello Rigo from the console!");
 };
+
+/*Cuando dice hacked my leg imaginen que es una pierna inteligente 🤣*/
